5a8b13c32468c390acc087495c0689898ddb4edb
Archy <archydevua@gmail.com>
2023-04-21 19:12:16 +0300

    use particle librarry & added exposion effect to asteroids

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem explosion;
     private void OnCollisionEnter2D(Collision2D col)
-    {
-        Debug.Log("Collision");
+    {   
         Destroy(gameObject);
+        Debug.Log("Collision");
+        Instantiate(explosion,transform.position,quaternion.identity);
+        
+        
     }
 }
